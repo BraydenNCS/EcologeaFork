dc5c91acfe3793b421c388cf776c9d0988f28ef5
Sean Tan <memberlite@gmail.com>
2024-11-05 11:08:39 +0800

    Backend-Simple-View-Works

--- index.js
+++ index.js
@@ -9,10 +9,16 @@ app.use(express.static("./public"));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/" + startPage);
 })
+
+const { viewBlogs } = require('./utils/BlogsUtils')
+
+app.get('/view-blogs', viewBlogs);
+
+
 server = app.listen(PORT, function () {
     const address = server.address();
     const baseUrl = `http://${address.address == "::" ? 'localhost' :
         address.address}:${address.port}`;
-    console.log(`Demo project at: ${baseUrl}`);
+    console.log(`Server running at: ${baseUrl}`);
 });
 module.exports = { app, server }
--- utils/BlogsUtils.js
+++ utils/BlogsUtils.js
@@ -1,1 +1,19 @@
-
+const { Blog } = require('../models/blog');
+const fs = require('fs').promises;
+async function readJSON(filename) {
+    try {
+        const data = await fs.readFile(filename, 'utf8');
+        return JSON.parse(data);
+    } catch (err) { console.error(err); throw err; }
+}
+async function viewBlogs(req, res) {
+    try {
+        const allPosts = await readJSON('utils/posts.json');
+        return res.status(201).json(allPosts);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+module.exports = {
+    viewBlogs
+};
