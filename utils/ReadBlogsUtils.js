1dcc7f4ae0832a67e68e0a64ff99d661705bf297
memberlite <memberlite@gmail.com>
2024-11-26 12:29:06 +0800

    fixed-some-bugs

--- utils/ReadBlogsUtils.js
+++ utils/ReadBlogsUtils.js
@@ -11,23 +11,25 @@ async function viewBlogs(req, res) {
         const allPosts = await readJSON('utils/posts.json');
         const filterDate = req.params.filter_date;
         if (filterDate == "" || filterDate == undefined) {
-            res.status(500).json({ "message": "Filter date cannot be empty" });
+            return res.status(500).json({ "message": "Filter date cannot be empty" });
         }
         const current_date = new Date();
         const selected_date = new Date(filterDate);
         const one_year_ago = new Date();
         one_year_ago.setFullYear(current_date.getFullYear() - 1);
         if (selected_date > current_date) {
-            res.status(500).json({ "message": "Filter date cannot be in the future" });
+            return res.status(500).json({ "message": "Filter date cannot be in the future" });
         }else if (selected_date < one_year_ago) {
-            res.status(500).json({ "message": "Filter date cannot be more than 1 year ago" });
+            return res.status(500).json({ "message": "Filter date cannot be more than 1 year ago" });
         }
-
+        if (isNaN(selected_date.getTime())) {
+            return res.status(400).json({ message: "Invalid date format" });
+        }
         const filteredPosts = filterDate != "default" ? allPosts.filter(post => post.date === filterDate) : allPosts;
 
-        res.status(200).json(filteredPosts);
+        return res.status(200).json(filteredPosts);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: error.message });
     }
 }
 
