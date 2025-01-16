6bcef8270012deec32fdf1255cfadb6c3c8adeef
memberlite <memberlite@gmail.com>
2025-01-16 11:17:54 +0800

    a

--- utils/UpdateBlogsUtils.js
+++ utils/UpdateBlogsUtils.js
@@ -41,7 +41,7 @@ async function updatePost(req, res) {
         return res.status(500).json({ message: error.message });
     }
 }
-//
+
 module.exports = {
     readJSON, writeJSON, updatePost
     };
